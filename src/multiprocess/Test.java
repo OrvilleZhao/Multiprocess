40021beb4393b1d91b8332500df2e5127da84a6c
ahnu120705097 <940725948@qq.com>
2016-11-12 16:45:36 +0800

    修改关键性bug

--- src/multiprocess/Test.java
+++ src/multiprocess/Test.java
@@ -11,19 +11,18 @@ import java.util.concurrent.ConcurrentLinkedQueue;
  *
  * @author Administrator
  */
-public class Multiprocess{
-
+public class Test{
     /**
      * @param args the command line arguments
      */
-    private static int route=5;
-    private static int coach=8;
-    private static int seat=100;
-    private static int station=10;
+    private static int route=1;
+    private static int coach=1;
+    private static int seat=10;
+    private static int station=4;
     private static ConcurrentLinkedQueue History=new ConcurrentLinkedQueue();
     public static void main(String[] args) {
       final TicketingDS tds=new TicketingDS(route,coach,seat,station);
-      int i=8000;
+      int i=10;
       while(i-->0){
       double chance=Math.random();
       if(chance>0.7&&chance<1)
@@ -34,8 +33,8 @@ public class Multiprocess{
             int s1=random(1,station-1);
             int s2=random(s1+1,station);
             Ticket k=tds.buyTicket("hhhh",r-1,s1-1,s2-1);
-            History.offer(k);
             if(k!=null){
+                History.offer(k);
                 System.out.println("成功订票:票的信息为:乘客姓名:"+k.passenger+" 车票编号:"+k.tid+" 列车车次:"+r+" 车厢编号:"+(k.coach+1)+" 座位号:"+(k.seat+1)+" 出发站编号:"+(k.departure+1)+" 终点站编号:"+(k.arrival+1));
             }
             else{
@@ -64,15 +63,15 @@ public class Multiprocess{
            new Thread(new Runnable(){
           @Override
           public void run() {
+              if(!History.isEmpty()){
               Ticket t=(Ticket)History.poll();
-              if(t!=null){
                     if(tds.refundTicket(t)){
                       System.out.println("您订购的编号为:"+t.tid+"的车票已经退订！");
                     }else{
                         System.out.println("错误的车票编号，请查询后再退订！");
                     }
-              }
-          }         
+                }
+           }         
          }).start();
      }
     } 
